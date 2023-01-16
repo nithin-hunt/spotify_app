a357f27797866fa030245cce3ca15cd2ee1cfea8
nithin-hunt <nithinpsgice@gmail.com>
2023-01-16 18:37:49 +0530

    Add get song by id route

--- routes/songs.js
+++ routes/songs.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const isAuthenticated = require('../middlewares/isAuthenticated');
 const Song = require('../models/songModel');
 const {validateSong} = require('../utils/validators');
 
 // Get all songs
-router.get("/", async(req,res) => {
+router.get("/", isAuthenticated, async(req,res) => {
     try {
-        const fetchSongs = await fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=india&api_key=50e64c2b34b804ba020864e65a1deb5c&format=json`)
+        const fetchSongs = await fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=india&api_key=${process.env.LASTFM_API_KEY}&format=json`)
         const songs = await fetchSongs.json();
         var songList =[]
         for (let i=0 ; i < songs.tracks.track.length; i++) {
@@ -30,8 +31,31 @@ router.get("/", async(req,res) => {
         }
         res.status(200).json(songList);
     } catch(e) {
-        return res.status(500).json({Error: e})
+        return res.status(400).json({Error: e})
     }
+})
+
+// Get song by Id
+router.get("/:id", isAuthenticated, async (req,res) => {
+    try {
+        const fetchSong = await fetch(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${process.env.LASTFM_API_KEY}&mbid=${req.params.id}&format=json`)
+        const song = await fetchSong.json();
+        
+        let getSong= {};
+        getSong.song_id = song.track.mbid;
+        getSong.name = song.track.name;
+        getSong.album = song.track.album.title;
+        getSong.artist = song.track.artist.name;
+        getSong.song = song.track.url;
+        getSong.image = song.track.album.image[0]['#text'];
+        getSong.duration = song.track.duration;
+        
+        return res.status(200).json(getSong);
+    } catch (e) {
+        console.log(e)
+        return res.status(400).json({Error: e});
+    }
+
 })
 
 module.exports = router;
